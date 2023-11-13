e9a7eee86b9cecf60f456eb57cfc604ca2e83a1f
leandrocgsi <leandrocgsi@gmail.com>
2023-11-13 17:54:39 -0300

    Migrating from ASP.NET Core 5 to ASP.NET 8

--- dot_net_8/RestWithASPNETUdemyRemovingDeprecations/RestWithASPNETUdemy/Controllers/AuthController.cs
+++ dot_net_8/RestWithASPNETUdemyRemovingDeprecations/RestWithASPNETUdemy/Controllers/AuthController.cs
@@ -22,7 +22,7 @@ namespace RestWithASPNETUdemy.Controllers
         [Route("signin")]
         public IActionResult Signin([FromBody] UserVO user)
         {
-            if (user == null) return BadRequest("Ivalid client request");
+            if (user == null) return BadRequest("Invalid client request");
             var token = _loginBusiness.ValidateCredentials(user);
             if (token == null) return Unauthorized();
             return Ok(token);
@@ -32,9 +32,9 @@ namespace RestWithASPNETUdemy.Controllers
         [Route("refresh")]
         public IActionResult Refresh([FromBody] TokenVO tokenVo)
         {
-            if (tokenVo is null) return BadRequest("Ivalid client request");
+            if (tokenVo is null) return BadRequest("Invalid client request");
             var token = _loginBusiness.ValidateCredentials(tokenVo);
-            if (token == null) return BadRequest("Ivalid client request");
+            if (token == null) return BadRequest("Invalid client request");
             return Ok(token);
         }
 
@@ -47,7 +47,7 @@ namespace RestWithASPNETUdemy.Controllers
             var username = User.Identity.Name;
             var result = _loginBusiness.RevokeToken(username);
 
-            if (!result) return BadRequest("Ivalid client request");
+            if (!result) return BadRequest("Invalid client request");
             return NoContent();
         }
     }
