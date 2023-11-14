70d5322d2b28b636f5ef7848c1e8707c4deac74c
leandrocgsi <leandrocgsi@gmail.com>
2023-11-13 23:13:51 -0300

    Migrating from ASP.NET Core 5 to ASP.NET 8

--- dot_net_8/RestWithASPNETUdemy/RestWithASPNETUdemy/Program.cs
+++ dot_net_8/RestWithASPNETUdemy/RestWithASPNETUdemy/Program.cs
@@ -1,3 +1,4 @@
+/* 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -21,3 +22,102 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+*/
+using EvolveDb;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using RestWithASPNETUdemy.Model.Context;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
+
+if (builder.Environment.IsDevelopment())
+{
+    MigrateDatabase(connection);
+}
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",
+        new Microsoft.OpenApi.Models.OpenApiInfo
+        {
+            Title = "REST API's From 0 to Azure with ASP.NET Core 5 and Docker",
+            Version = "v1",
+            Description = "API RESTful developed in course 'REST API's From 0 to Azure with ASP.NET Core 5 and Docker'",
+            Contact = new Microsoft.OpenApi.Models.OpenApiContact
+            {
+                Name = "Leandro Costa",
+                Url = new Uri("https://github.com/leandrocgsi")
+            }
+        });
+});
+
+builder.Services.AddCors(options => options.AddDefaultPolicy(builder =>
+{
+    builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+}));
+
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",
+        new Microsoft.OpenApi.Models.OpenApiInfo
+        {
+            Title = "REST API's From 0 to Azure with ASP.NET Core 5 and Docker",
+            Version = "v1",
+            Description = "API RESTful developed in course 'REST API's From 0 to Azure with ASP.NET Core 5 and Docker'",
+            Contact = new Microsoft.OpenApi.Models.OpenApiContact
+            {
+                Name = "Leandro Costa",
+                Url = new Uri("https://github.com/leandrocgsi")
+            }
+        });
+});
+
+// Add your services, middleware, and configurations here...
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
+}
+
+app.UseHttpsRedirection();
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+void MigrateDatabase(string connection)
+{
+    try
+    {
+        var evolveConnection = new MySql.Data.MySqlClient.MySqlConnection(connection);
+        var evolve = new Evolve.Evolve(evolveConnection, msg => Log.Information(msg))
+        {
+            Locations = new List<string> { "db/migrations", "db/dataset" },
+            IsEraseDisabled = true,
+        };
+        evolve.Migrate();
+    }
+    catch (Exception ex)
+    {
+        Log.Error("Database migration failed", ex);
+        throw;
+    }
+}
